0712acafbcb92dcbbd4166e7d84df0e222dc3b3a
cahyaayaa <chyanna790@gmail.com>
2025-03-19 22:01:56 +0700

    first commit

---
+++ src/projectt1/ProjectT1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectt1;
+
+/**
+ *   NAMA  : ANNA CAHYA NNINGRUM
+ *   NIM   : 202457201013
+ *   PRODI : SISTEM INFORMASI - A
+ */
+public class ProjectT1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+        System.out.println("Hello Anna");
+        System.out.println("Hello Cahya");
+        System.out.println("Hello Ningrum");
+    }
+    
+}
